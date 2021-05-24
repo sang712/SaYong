e14fc4430b24362c1db5f01d9fbb953c47263bc8
sang712 <tkdrlf95@gmail.com>
2021-05-25 01:34:17 +0900

    serializer로 follower에 접근 구현, 로그아웃 구현

--- final-pjt-back/accounts/serializers.py
+++ final-pjt-back/accounts/serializers.py
@@ -1,18 +1,22 @@
 from django.contrib.auth import get_user_model
+from django.urls.conf import include
 from rest_framework import serializers
 from .models import History
 
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    # followings = serializers.IntegerField()
     
     class Meta:
         model = get_user_model()
-        # fields = '__all__'
-        fields = ('username', 'password')
+        # fields = include('username', 'password')
+        exclude = ('last_login', 'is_superuser', 'is_staff', 'is_active', 'date_joined', 'groups', 'user_permissions')
         # 필드에 추가하길 고려할만한 것: 이메일 주소, 지역, 계정과 이름의 구분 등
-
-
+        # 무한루프에 빠지지 않게 깊이 1을 추가함
+        depth = 1
+   
+        
 class HistorySerializer(serializers.ModelSerializer):
     
     class Meta:
